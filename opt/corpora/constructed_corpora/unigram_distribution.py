2e31ba4d5e95afc68eee9f24552c6c89ffb5a147
marbou090 <rikka2217@gmail.com>
2023-10-12 17:14:13 +0900

    fix unigram path

--- opt/corpora/constructed_corpora/unigram_distribution.py
+++ opt/corpora/constructed_corpora/unigram_distribution.py
@@ -3,18 +3,28 @@ import hashlib
 import numpy as np
 import torch
 import sys
-sys.path.insert(0,'/u/scr/isabelvp/multilingual-transfer/salesforce-awd-lm')
+import os
+import argparse
 
-import data
+this_file_path = os.path.join(os.getcwd(), __file__)
+project_path = os.path.split(os.path.split(os.path.split(this_file_path)[0])[0])[0]
+print(project_path)
+sys.path.insert(0, project_path)
 
+from corpora.data import Corpus
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--lang", type=str, default="es", help="corpus to base this one off of (length, vocab size etc)")
+args = parser.parse_args()
+print(args)
 # Language to base the distribution on. Should not be that important.
-lang = "es"
 
-lang_location = f"/u/scr/nlp/isabelvp/wiki-multilingual/{lang}/{lang}wiki-corpus-"
-fn = '/u/scr/isabelvp/multilingual-transfer/salesforce-awd-lm/sf_corpora/onelang/corpus.{}.culldata'.format(hashlib.md5(lang_location.encode()).hexdigest())
-lang_corpus = torch.load(fn)
 
-uni_corpus = data.Corpus()
+lang_fn = os.path.join(project_path, "corpora", "pickled_files", f"corpus-{args.lang}.cull")
+print(f"loading original lang corpus from {lang_fn}")
+lang_corpus = torch.load(lang_fn)
+
+uni_corpus = Corpus()
 uni_corpus.dictionary = lang_corpus.dictionary
 word_indices, freq = list(zip(*lang_corpus.dictionary.counter.items()))
 freq = np.array(freq)
@@ -29,5 +39,5 @@ uni_corpus.test = torch.LongTensor(
 
 print(f"Made train/valid/test, train_length is {len(uni_corpus.train)}")
 
-save_fn = '/u/scr/isabelvp/multilingual-transfer/salesforce-awd-lm/sf_corpora/onelang/corpus.{}.culldata'.format(hashlib.md5(f"unigram-{lang}".encode()).hexdigest())
+save_fn = os.path.join(project_path, "corpora", "pickled_files", f"corpus-unigram-{args.lang}.cull") 
 torch.save(uni_corpus, save_fn)
